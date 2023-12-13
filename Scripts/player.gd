9fbf8c89442a25ca829dc85be2a88de770eed7e0
bob <bobovo@gmail.com>
2023-12-13 01:27:01 -0400

    add laser that's fire continuously

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,15 +4,23 @@ signal laser_shot(laser_scene, location)
 
 @export var speed = 300
 
+@export var rate_of_fire := 0.25
+
 @onready var muzzle = $Muzzle
 
 var laser_scene = preload("res://Scenes/laser.tscn")
 
+var shoot_cd := false
+
 @warning_ignore("unused_parameter")
 
 func _process(delta):
-	if Input.is_action_just_pressed("shoot"):
-		shoot()
+	if Input.is_action_pressed("shoot"):
+		if !shoot_cd:
+			shoot_cd = true
+			shoot()
+			await get_tree().create_timer(rate_of_fire).timeout
+			shoot_cd = false
 
 func _physics_process(delta):
 	var direction = Vector2(Input.get_axis("move_left",
