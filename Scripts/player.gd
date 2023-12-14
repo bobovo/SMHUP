4c84c9f3e5ee6b4db8668d1aa695622b112da0d5
bob <bobovo@gmail.com>
2023-12-14 03:18:27 -0400

    add bounderies to the game player can't go off the screen no more

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -26,6 +26,7 @@ func _physics_process(delta):
 	#print(direction)
 	velocity = direction * speed
 	move_and_slide()
+	global_position = global_position.clamp(Vector2.ZERO, get_viewport_rect().size)
 
 func shoot():
 	laser_shot.emit(laser_scene, muzzle.global_position)
